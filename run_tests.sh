0446ab56fd801a1b2a128aa6c855186d33d52ff3
jorgelp-code <jorge.lp@icjardin.com>
2025-12-19 09:52:10 +0100

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-pytest -q
+
+python3 - << EOF
+from solucion import suma
+
+errors = 0
+
+if suma(2, 3) != 5:
+    print("Error: suma(2,3) != 5")
+    errors += 1
+
+if suma(-1, 1) != 0:
+    print("Error: suma(-1,1) != 0")
+    errors += 1
+
+if errors == 0:
+    print("Todos los tests superados")
+    exit(0)
+else:
+    exit(1)
+EOF
